9911aca5d3eabb616deacbca32c9dc79b85667d8
Aleksander Szachorin <aleksander.szachorin@gmail.com>
2023-04-19 12:56:22 +0300

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,6 +8,7 @@ const Button = () => {
         className="btn text-white text-md font-bold py-4 px-6 rounded-md cursor-pointer"
         target="_blank"
         onclick="gtag('event', 'conversion', {'send_to': '948922106/MQU4CJGkhZkYEPrNvcQD'});"
+        ref="conversion"
       >
         Spróbuj bezpłatnie
       </a>
