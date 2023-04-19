4fff3e314e72f6185d5d17b67f46e9e9d26f1a8e
Aleksander Szachorin <aleksander.szachorin@gmail.com>
2023-04-19 12:58:17 +0300

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,8 +7,6 @@ const Button = () => {
         href="https://t.me/chatGPTPolskaBot"
         className="btn text-white text-md font-bold py-4 px-6 rounded-md cursor-pointer"
         target="_blank"
-        onclick="gtag('event', 'conversion', {'send_to': '948922106/MQU4CJGkhZkYEPrNvcQD'});"
-        ref="conversion"
       >
         Spróbuj bezpłatnie
       </a>
