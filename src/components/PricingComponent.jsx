e7eda943706ad6230019baa3e011353fdac0412b
Aleksander Szachorin <aleksander.szachorin@gmail.com>
2023-05-19 22:00:22 +0300

    Update PricingComponent.jsx

--- src/components/PricingComponent.jsx
+++ src/components/PricingComponent.jsx
@@ -11,14 +11,14 @@ function PricingComponent() {
 
   return (
     <section className="section">
-      <div className="flex flex-col md:flex-row justify-center p-6 ">
-        <div className="flex flex-col bg-white  rounded p-4 m-2 w-1/3 bg-gray-100">
+      <div className="flex flex-col md:flex-row justify-center p-6 w-full ">
+        <div className="flex flex-col bg-white  rounded p-4 m-2 lg:w-1/3 bg-gray-100">
           <h2 className="text-lg font-semibold mb-2">Free</h2>
           <button className="mt-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             Get Started
           </button>
         </div>
-        <div className="flex flex-col bg-white border-4 border-blue-500 rounded p-4 m-2 w-1/3">
+        <div className="flex flex-col bg-white border-4 border-blue-500 rounded p-4 m-2 lg:w-1/3">
           <h2 className="text-lg font-semibold mb-2">Premium</h2>
           <p className="text-xl font-bold mb-4">{pricing[period]}</p>
           <ul className="list-disc list-inside">
